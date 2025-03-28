be99810322b2354d1010830147f5c9d53bb2256a
CZandt <95643051+CZandt@users.noreply.github.com>
2025-03-27 20:05:43 -0600

    MainPush

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,31 @@
 #include <AnalogSensor.h>
 #include <DataCollector.h>
 
-constexpr bool THROTTLE_1_CRITICALITY = true;
+constexpr bool THROTTLE_1_CRITICALITY = false;
 constexpr uint8_t THROTTLE_1_PIN = 18;
 
-constexpr bool THROTTLE_2_CRITICALITY = true;
+constexpr bool THROTTLE_2_CRITICALITY = false;
 constexpr uint8_t THROTTLE_2_PIN = 19;
 
-constexpr uint32_t THROTTLE_INTERVAL = 15;
+constexpr uint32_t THROTTLE_INTERVAL = 100;
 
 AnalogSensor throttle1 = AnalogSensor(ReservedIDs::Throttle1PositionId, THROTTLE_1_CRITICALITY, THROTTLE_1_PIN, THROTTLE_INTERVAL);
-AnalogSensor throttle2 = AnalogSensor(ReservedIDs::Throttle2PositionId, THROTTLE_1_CRITICALITY, THROTTLE_1_PIN, THROTTLE_INTERVAL);
+AnalogSensor throttle2 = AnalogSensor(ReservedIDs::Throttle2PositionId, THROTTLE_1_CRITICALITY, THROTTLE_2_PIN, THROTTLE_INTERVAL);
 
-constexpr bool BRAKE_CRITICALITY = true;
+constexpr bool BRAKE_CRITICALITY = false;
 constexpr uint8_t BRAKE_PIN = 20;
-constexpr uint32_t BRAKE_INTERVAL = 15;
+constexpr uint32_t BRAKE_INTERVAL = 100;
 
 AnalogSensor brake = AnalogSensor(ReservedIDs::BrakePressureId, BRAKE_CRITICALITY, BRAKE_PIN, BRAKE_INTERVAL);
 
-constexpr bool SWITCH_CRITICALITY = true;
-constexpr uint8_t SWITCH_PIN = 21;
-constexpr uint32_t SWITCH_INTERVAL = 15;
+constexpr bool SWITCH_CRITICALITY = false;
+constexpr uint8_t SWITCH_PIN = 16;
+constexpr uint32_t SWITCH_INTERVAL = 100;
 
 DigitalSensor startSwitch = DigitalSensor(ReservedIDs::StartSwitchId, SWITCH_CRITICALITY, SWITCH_PIN, SWITCH_INTERVAL);
 
 
-// constexpr bool DATA_CRITICALITY = true;
+// constexpr bool DATA_CRITICALITY = false;
 // constexpr uint8_t DATA_PIN = 13; // TBD
 // constexpr uint32_t DATA_INTERVAL = 100;
 
@@ -41,7 +41,7 @@ Sensor* SENSORS[] = {
     &brake,
     &startSwitch,
 };
-constexpr bool DEBUG = false;
+constexpr bool DEBUG = true;
 FlexCAN_T4<CAN1, RX_SIZE_256> motorCAN;
 FlexCAN_T4<CAN2, RX_SIZE_256> dataCAN;
 
@@ -57,6 +57,7 @@ void setup() {
     dataCAN.begin();
     dataCAN.setBaudRate(CAN_BAUD_RATE);
     dc.begin(&motorCAN, &dataCAN);
+    Serial.println("START");
 }
 
 void loop() {
